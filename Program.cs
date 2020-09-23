bb265b282806688a3f259a10f7bd5386a872e1fb
Bernard <bernardtelgon@gmail.com>
2020-09-23 16:50:07 -0300

    csharp exercises

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Authentication;
+
+namespace bernardVisual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de área");
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
